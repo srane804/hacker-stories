9c09085e2eaf6d80bfb1b8e5ee3f68c1b8db856c
Saurabh Rane <Saurabh@innovisk.global>
2024-05-24 16:50:45 +0530

    New changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,73 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const welcome = {
+    greeting:'Hey',
+    title: 'React'
+};
+
+function getTitle()
+{
+  return welcome.title ;
+}
+
+const list = [
+  {
+    title:'Angular',
+    url:'https://angular.js.org/',
+    author:'Dan Abramov, Andrew Clark',
+    num_comments:5,
+    points:1,
+    objectId:0
+  },
+  {
+    title:'React',
+    url:'https://reactjs.org/',
+    author:'Jordan Walke',
+    num_comments:3,
+    points:4,
+    objectId:1
+  },
+  {
+    title:'Redux',
+    url:'https://redux.js.org/',
+    author:'Dan Abramov, Andrew Clark',
+    num_comments:2,
+    points:5,
+    objectId:2
+  }
+]
+
+const numbers = [0,1,2,3,4];
+const initialValue = 0;
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
+      <h1>My hacker stories</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text"/>
+      <hr/>
+
+      <ul>
+        {list.map(function (item){
+            return <li key={item.objectId}>
+              <span>
+              <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>,
+              <span>{item.num_comments}</span>
+              </li>
+        })}
+      </ul>
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {/*reduce method */}
+        <h1>{numbers.reduce((accumulator, currentValue)=>accumulator+currentValue, initialValue)}</h1>       
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
